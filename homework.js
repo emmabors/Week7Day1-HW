f6423373373b0014512e114c031eb4c882b84a94
Emma Borsavage <emma_borsavage@aol.com>
2023-03-18 21:20:14 -0600

    added codewars problems

--- homework.js
+++ homework.js
@@ -39,3 +39,30 @@ console.log(replaceEvens(arr))
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
 
+// codewars #1//
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  var splitString = str.split('');
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join('');
+  return joinArray
+}
+
+console.log(solution('world'))
+console.log(solution('word'))
+
+// codewars #2//
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't
+
+function lovefunc(flower1,flower2){
+    if (flower1 % 2 == 0&&flower2 % 2 !== 0) {
+        return true
+    }else if(flower1 % 2 !== 0&&flower2 % 2 == 0){
+        return true
+    }else return false
+}
