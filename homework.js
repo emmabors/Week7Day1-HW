3fb6454ac52a33f387be358051858068fa906676
Emma Borsavage <emma_borsavage@aol.com>
2023-03-18 18:56:41 -0600

    homework

---
+++ homework.js
@@ -0,0 +1,41 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+`Matched ${dog_name}` if name is in the string, if its not a match console.log "No Match"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(dog_names, dog_string){
+    for(let name of dog_names){
+        if (dog_string.includes(name)){ 
+            return `Matched ${name}`
+    } else {
+            return `No Match`
+    }
+    }   
+}
+    
+console.log(findWords(dog_names,dog_string))
+
+//============Exercise #2 ============//
+/*Write a function that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0){
+            console.log(i);
+        } 
+    }
+return arr
+}
+
+console.log(replaceEvens(arr))
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
